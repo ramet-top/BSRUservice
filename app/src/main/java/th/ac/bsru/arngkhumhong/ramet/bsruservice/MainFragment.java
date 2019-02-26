54b3cd859893b9d19007722ba42b70676e200a82
sunzerozaza <sunzamahachai@gmail.com>
2019-02-26 16:07:13 +0700

    finish 26Feb

--- app/src/main/java/th/ac/bsru/arngkhumhong/ramet/bsruservice/MainFragment.java
+++ app/src/main/java/th/ac/bsru/arngkhumhong/ramet/bsruservice/MainFragment.java
@@ -2,10 +2,12 @@ package th.ac.bsru.arngkhumhong.ramet.bsruservice;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,32 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    // Register Control
+
+        registerControl();
+
+    }   //Main Method
+
+    private void registerControl() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //  Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
